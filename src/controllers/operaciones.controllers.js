516c3405eddcd83a87e4d36fe2e2f46faa5637f3
Santiago <albarracinsantiago24@gmail.com>
2021-06-22 11:52:58 -0300

    Casi listo

--- src/controllers/operaciones.controllers.js
+++ src/controllers/operaciones.controllers.js
@@ -16,9 +16,29 @@ operacionesCtrl.renderOperaciones = (req, res) => {
         return;
       }
       // console.log('usuario', resultado);
-      res.render("operaciones/nueva-operacion", { user: usuario });
+      const getMovimiento = (coleccion, client) => {
+        coleccion.find({
+          $or: [
+                    { usuarioOrigen: usuario },
+                    { usuarioDestino: usuario }
+                 ]
+        }, function (err, movimientos) {
+          client.close();
+          if (err) {
+            console.log("Hubo un error al consultar:", err);
+            res.send("Hubo un error");
+            return;
+          }
+          console.log('movimientos', movimientos);
+          res.render("operaciones/nueva-operacion", { user: usuario, movimientos });
+          
+        });
+      }
+      
+      connectiondb(getMovimiento, 'movimientos');
     });
   }
+
   connectiondb(getUser, 'usuarios');
 
 };
@@ -46,10 +66,10 @@ operacionesCtrl.createNewOperacion = (req, res) => {
 };
 
 operacionesCtrl.newTransfer = (req, res) => {
-  console.log(req.body);
-  const usuarioOrigen = req.session.usuario;
-  let usuarioDestino = null
-  const obtenerUsuarioDestino = async (collecion, client) => {
+  try {
+    const usuarioOrigen = req.session.usuario;
+    console.log('usuarioOrigen', usuarioOrigen);
+  const obtenerUsuarioDestino = (collecion, client) => {
     collecion.findOne({ 'aliasCuenta': req.body.usuarioDestino }, function (err, resultado) {
       client.close();
       if (err) {
@@ -57,46 +77,64 @@ operacionesCtrl.newTransfer = (req, res) => {
         res.send("Hubo un error");
         return;
       }
-      console.log('Resultado', resultado);
-      usuarioDestino = resultado
+      const usuarioDestino = resultado
+      console.log('usuarioDestino', usuarioDestino);
+      const updateUserDestino = (collecion, client) => {
+        collecion.updateOne({ _id: mongodb.ObjectID(usuarioDestino._id) }, {
+          $inc: {
+            saldo: parseInt(req.body.monto)
+          }
+        }, function (err, resultado) {
+          client.close();
+          if (err) {
+            console.log("Hubo un error al consultar:", err);
+            res.send("Hubo un error");
+            return;
+          }
+          console.log('usuarioActualizado', resultado);
+          const updateUserOrigen = (collecion, client) => {
+            collecion.updateOne({ _id: mongodb.ObjectID(usuarioOrigen._id) }, {
+              $inc: {
+                saldo: -parseInt(req.body.monto)
+              }
+            }, function (err, resultado) {
+              client.close();
+              if (err) {
+                console.log("Hubo un error al consultar:", err);
+                res.send("Hubo un error");
+                return;
+              } console.log('usuarioActualizado', resultado);
+
+              const movimiento = {
+                usuarioOrigen,
+                usuarioDestino,
+                monto: parseInt(req.body.monto)
+              }
+              const createMovimiento = (collecion, client) => {
+                collecion.insertOne(movimiento, function (err, resultado) {
+                  client.close();
+                  if (err) {
+                    console.log("Hubo un error al consultar:", err);
+                    res.send("Hubo un error");
+                    return;
+                  } console.log('movimientoCreado', resultado);
+                  res.redirect('home');
+                });
+              }
+              connectiondb(createMovimiento, 'movimientos');
+            });
+          }
+          connectiondb(updateUserOrigen, 'usuarios');
+        });
+      }
+      connectiondb(updateUserDestino, 'usuarios');
     });
   }
   connectiondb(obtenerUsuarioDestino, 'usuarios');
-console.log(usuarioDestino);
-const updateUserDestino = (collecion, client) => {
-    collecion.updateOne({ _id: mongodb.ObjectID(usuarioDestino._id) }, {
-      $inc: {
-        saldo: parseInt(req.body.monto)
-      }
-    }, function (err, resultado) {
-      client.close();
-      if (err) {
-        console.log("Hubo un error al consultar:", err);
-        res.send("Hubo un error");
-        return;
-      }
-    });
-  }
-  connectiondb(updateUserDestino, 'usuarios');
-
-const updateUserOrigen = (collecion, client) => {
-    collecion.updateOne({ _id: mongodb.ObjectID(usuarioOrigen._id) }, {
-      $desc: {
-        saldo: parseInt(req.body.monto)
-      }
-    }, function (err, resultado) {
-      client.close();
-      if (err) {
-        console.log("Hubo un error al consultar:", err);
-        res.send("Hubo un error");
-        return;
-      }
-    });
-  }
-  connectiondb(updateUserOrigen, 'usuarios');
-
-
-  res.send("render movimientos");
+  } catch (error) {
+    console.log('error', error);
+  } 
+  
 };
 
 operacionesCtrl.renderEditarOperaciones = (req, res) => {
